63f76c03201441064b50649a8cd0744daed12d6c
Kätlin Selberg <katlin.selberg@etsnord.ee>
2023-01-28 15:09:56 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// This is a comment to my code
+
+// Rakendus küsib kasutajal sisestada tema nime
+// Rakendus tervitab kasutajat nime pidi
+
+Console.WriteLine("Enter your name"); //Output
+// string - sõne
+String userName = Console.ReadLine(); //Input
+
+
+Console.WriteLine("Hello" + ", "+ 
+    userName+"!");  // Output
+// String interpolation
+Console.WriteLine($"Hello, {userName} !");  //Output
